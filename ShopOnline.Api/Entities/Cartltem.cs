2cfbfb06b3f61e410a59f6a2e0a26019f5f9a4ee
tiamo2086 <nqv0938640601@gmail.com>
2023-01-03 11:10:28 +0700

    ShopOnline

---
+++ ShopOnline.Api/Entities/Cartltem.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopOnline.Api.Entities
+{
+    public class Cartltem
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int productId { get; set; }
+        public int Qty { get; set; }
+    }
+}
